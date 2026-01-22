8885936cbe84b7b7e95263fadec75d48e14c29f9
serraguti <pacoserranox@gmail.com>
2026-01-22 19:10:24 +0100

    up

---
+++ python02usuario.py
@@ -0,0 +1,9 @@
+print("Ejemplo información del usuario")
+#Preguntar al usuario algo
+print("Dame un numero")
+#Almacenar el dato que nos ha dado en una variable
+numero1 = input()
+print("Dame otro numero")
+numero2 = input()
+suma = int(numero1) + int(numero2)
+print("La suma es ", suma)
