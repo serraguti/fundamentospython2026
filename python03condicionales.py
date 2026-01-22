c04907179c47c670f49c23f10ab02e56321cc02b
serraguti <pacoserranox@gmail.com>
2026-01-22 19:44:02 +0100

    aaq

---
+++ python03condicionales.py
@@ -0,0 +1,9 @@
+print("Ejemplo condicionales")
+edad = 8
+#Preguntamos si es mayor de edad
+if (edad >= 18):
+    print("Es mayor de edad")
+else:
+    print("Es menor de edad")
+#Despues del IF
+print("Fin de programa")
---
+++ python04positivonegativo.py
@@ -0,0 +1,10 @@
+print("Positivo/Negativo/Cero")
+print("Introduzca un numero")
+num = int(input())
+if (num > 0):
+    print("Positivo")
+elif (num == 0):
+    print("Cero")
+else:
+    print("Negativo")
+print("Fin de programa")
