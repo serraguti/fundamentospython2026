6f8747bf75d1ab8b75efc9811d764e2b4bd1b6f2
serraguti <pacoserranox@gmail.com>
2026-01-22 20:02:24 +0100

    up mayor

---
+++ python05mayor.py
@@ -0,0 +1,12 @@
+print("Numero mayor")
+print("Introduzca numero 1")
+num1 = int(input())
+print("Introduzca numero 2")
+num2 = int(input())
+if (num1 > num2):
+    print ("El mayor es ", num1)
+elif (num1 == num2):
+    print("Los dos son iguales")
+else:
+    print("El mayor es ", num2)
+print("Fin de programa")
