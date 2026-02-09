de344234f1be09992ad8eed89830d637002a2452
serraguti <pacoserranox@gmail.com>
2026-02-09 16:52:55 +0100

    up consultas accion

---
+++ ORACLE/05 - CONSULTAS ACCION.sql
@@ -0,0 +1,36 @@
+select * from PLANTILLA;
+select max(EMPLEADO_NO) + 1 from PLANTILLA;
+--9902
+commit;
+insert into PLANTILLA
+(HOSPITAL_COD, SALA_COD, APELLIDO
+, FUNCION, TURNO, SALARIO, EMPLEADO_NO)
+values (22, 6, 'Super Lopez', 'ENFERMERA', 'T', 150000
+, (select max(EMPLEADO_NO) + 1 from PLANTILLA));
+delete from PLANTILLA;
+ROLLBACK;
+delete from PLANTILLA where apellido='Super Lopez';
+--Eliminar toda la plantilla del hospital El Carmen
+delete from PLANTILLA where HOSPITAL_COD=
+(select HOSPITAL_COD from HOSPITAL where NOMBRE='El Carmen');
+--subir en 1 el salario de la plantilla (todos)
+update PLANTILLA set SALARIO = SALARIO + 1;
+update PLANTILLA set funcion='ENFERMERA', SALARIO=SALARIO + 1
+where FUNCION='ENFERMERO';
+update PLANTILLA set SALARIO=
+(select SALARIO from PLANTILLA where APELLIDO='karplus w.')
+where SALA_COD=4;
+select SALARIO from PLANTILLA where APELLIDO='karlus w.';
+--Mostrar todas las personas de la PLANTILLA de la 
+--sala de psiquiatria
+select * from PLANTILLA
+where SALA_COD in
+(select SALA_COD from SALA where NOMBRE='psiquiatria');
+select SALA_COD from SALA where NOMBRE='psiquiatria';
+--Modificar el TURNO a Mañana a todos los de la plantilla
+--de la sala de Psiquiatría
+update PLANTILLA set TURNO='M'
+where SALA_COD in 
+(select SALA_COD from SALA where NOMBRE='psiquiatria');
+select SALA_COD from SALA where NOMBRE='psiquiatria';
+select * from SALA;
--- ORACLE/test.sql
+++ ORACLE/test.sql
@@ -1,1 +1,2 @@
-select * from DEPT;
+select * from HOSPITAL;
+
